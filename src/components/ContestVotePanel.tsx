4053f7fe82d43688b9693d064767f60e3d933b36
George Tan <ggtan@ggtan.net>
2022-10-28 00:26:25 -0400

    User can reset votes

--- src/components/ContestVotePanel.tsx
+++ src/components/ContestVotePanel.tsx
@@ -1,6 +1,5 @@
-import { Key } from "react";
-import { Component } from "react";
-import { Submission } from "./ContestDetails";
+import React, { Component } from "react";
+import { Submission, Vote } from "./ContestDetails";
 import './ContestVotePanel.css';
 import { Contest } from "./ContestList";
 
@@ -14,6 +13,8 @@ export type Rank = typeof RANKS[number]
 
 type ContestVotePanelProps = {
   onRankClick: (rank: Rank) => void | Promise<void>
+  onResetVotesClick: () => void
+  votes: Vote[] | undefined
 }
 
 type ContestVotePanelState = {
@@ -24,6 +25,8 @@ export default class ContestVotePanel extends Component<ContestVotePanelProps, C
   constructor(props: ContestVotePanelProps) {
     super(props)
     this.state = {}
+
+    this.resetVotes = this.resetVotes.bind(this)
   }
 
   clickRankHandler(rank: Rank) {
@@ -31,10 +34,30 @@ export default class ContestVotePanel extends Component<ContestVotePanelProps, C
     this.props.onRankClick(rank)
   }
 
+  showVote(rank: Rank): boolean {
+    if (!this.props.votes) {
+      return false;
+    }
+
+    for (const vote of this.props.votes) {
+      if (vote.rank == rank) {
+        return false
+      }
+    }
+
+    return true
+  }
+
+  resetVotes() {
+    this.props.onResetVotesClick()
+    this.setState({ selectedRank: undefined })
+  }
+
   render() {
     return (
-      <div>
+      <React.Fragment>
         {RANKS.map((rank) =>
+          this.showVote(rank) &&
           <button
             key={rank}
             className={rank + "Button " + (this.state.selectedRank === rank ? "selected" : "")}
@@ -42,7 +65,8 @@ export default class ContestVotePanel extends Component<ContestVotePanelProps, C
             {rank}
           </button>
         )}
-      </div>
+        <button className="reset-votes-button" onClick={this.resetVotes}>Reset votes</button>
+      </React.Fragment>
     )
   }
 }
