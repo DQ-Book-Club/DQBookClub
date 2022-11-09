1f1c8ba1429fba824fd90ac0765eb72082b8654b
Tomer Kimia <tomer.kimia@yum.com>
2022-11-08 22:33:41 -0600

    Upload shows progress of uploading the photo

--- src/components/contestdetails/OpenContestDetails.tsx
+++ src/components/contestdetails/OpenContestDetails.tsx
@@ -1,5 +1,5 @@
-import { getDownloadURL, ref, uploadBytes } from "firebase/storage";
-import React, { ChangeEvent } from "react";
+import { getDownloadURL, ref, uploadBytesResumable } from "firebase/storage";
+import React, { ChangeEvent, useState } from "react";
 import { auth, db, storage } from "../../services/firebaseServices";
 import './ContestDetails.css'
 import ContestSubmission from "../ContestSubmission";
@@ -15,21 +15,34 @@ type OpenContestDetailsProps = {
 
 export default function OpenContestDetails(props: OpenContestDetailsProps) {
   const userFolder = ref(storage, auth.currentUser?.uid)
+  const [uploadPhase, setUploadPhase] = useState<string | undefined>()
+  const [uploadProgress, setUploadProgress] = useState<number | undefined>()
+
   const submission = props.submissions
       ?.filter(submission => submission.id === auth.currentUser!.uid)
       ?.at(0)
 
   async function submitToContest(event: ChangeEvent<HTMLInputElement>) {
     if (!event.target.files || event.target.files.length !== 1) return
-
+    
+    setUploadPhase('Uploading...')
+    setUploadProgress(0)
     const file = event.target.files[0]
     const fileRef = ref(userFolder, props.contest.id)
-    const uploadResult = await uploadBytes(fileRef, file)
-    const imageUrl = await getDownloadURL(uploadResult.ref)
+    const uploadTask = uploadBytesResumable(fileRef, file)
 
-    await setDoc(
-      doc(db, "contests", props.contest.id, "submissions", auth.currentUser!.uid),
-      { imageUrl }
+    uploadTask.on('state_changed',
+      (snapshot) => setUploadProgress(snapshot.bytesTransferred / snapshot.totalBytes),
+      (err) => { setUploadProgress(undefined); throw err },
+      async () => {
+        setUploadPhase('Updating your submission...')
+        const imageUrl = await getDownloadURL(uploadTask.snapshot.ref)
+        await setDoc(
+          doc(db, "contests", props.contest.id, "submissions", auth.currentUser!.uid),
+          { imageUrl }
+        )
+        setUploadProgress(undefined)
+      }
     )
   }
 
@@ -45,7 +58,7 @@ export default function OpenContestDetails(props: OpenContestDetailsProps) {
         />
       }
       </div>
-      <ImageUploadButton onUploadFile={submitToContest} />
+      <ImageUploadButton onUploadFile={submitToContest} progressText={uploadPhase} progress={ uploadProgress } />
     </React.Fragment>
   )
 }
